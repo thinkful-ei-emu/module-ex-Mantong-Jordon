c48fd281e75d8b7f5b72d09d0072c86c6b147c75
Jvymvn <Jordon.carter@yahoo.com>
2019-06-10 15:03:19 -0400

    namespace #3

--- scripts/store.js
+++ scripts/store.js
@@ -12,11 +12,51 @@ const store = (function() {
   let hideCheckedItems = false;
   let searchTerm = '';
 
+    function findById(id){
+      return store.items.find(item => item.id === id);
+    }
+
+    function addItem(name){
+      try{
+        Item.validateName(name);
+        this.items.push(Item.create(name));
+      }
+      catch(error){
+        console.log(`${error.message}`);
+      }
+    }
+
+    function findAndToggleChecked(id){
+      let toggle = this.findById(id);
+      toggle.checked = true;
+    }
+
+    function findAndUpdateName(id, newName){
+      try{
+        Item.validateName(newName);
+        let update = this.findById(id);
+        update.name = newName;
+      }
+      catch(error){
+        console.log(`Cannot update name: ${error.message}`);
+      }
+    }
+
+    function findAndDelete(id){
+      let filteredItem = store.items.filter(word => word.id === id);
+      store.items.splice(filteredItem, 1);
+      //In console, run shoppinglist.render() after 
+    }
+
   return {
     hideCheckedItems,
     searchTerm,
-    items
-
+    items,
+    findById,
+    addItem,
+    findAndToggleChecked,
+    findAndUpdateName,
+    findAndDelete
   };
 
 }());
